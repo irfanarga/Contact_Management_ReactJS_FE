25b3a960eb353db1e5867b5a0a5cece06e995ac8
irfanarga <irfanargaprasetya81@gmail.com>
2025-10-03 09:40:52 +0700

    add login user page

--- src/lib/api/UserApi.js
+++ src/lib/api/UserApi.js
@@ -8,3 +8,14 @@ export const userRegister = async ({ username, password, name }) => {
     body: JSON.stringify({ username, password, name }),
   });
 };
+
+export const userLogin = async ({ username, password }) => {
+  return await fetch(`${import.meta.env.VITE_API_PATH}/users/login`, {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/json",
+      Accept: "application/json",
+    },
+    body: JSON.stringify({ username, password }),
+  });
+};
